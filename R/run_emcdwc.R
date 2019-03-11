5cf0c853c9cb1bfc3c0a5e8024d063014d039cd9
SHOJINAGA Ryan <rshojin@deq.state.or.us>
2019-03-11 07:17:11 -0700

    Update run_emcdwc.R

--- R/run_emcdwc.R
+++ R/run_emcdwc.R
@@ -3,17 +3,17 @@
 options(stringsAsFactors = FALSE)
 
 # LOAD DATA ----
-wqFil <- 'D:/siletz/calib/wq'
+wqFil <- 'C:/siletz/calib/wq'
 
-sapply(c('D:/siletz/scripts/R/proc_flow_4_wq.R',
-         'D:/siletz/scripts/R/proc_qlc.R',
-         'D:/siletz/scripts/R/reduce_qlc.R',
-         'D:/siletz/scripts/R/proc_routing.R'),
+filDir <- wqFil
+
+sapply(c('C:/siletz/scripts/R/proc_flow_4_wq.R',
+         'C:/siletz/scripts/R/proc_qlc.R',
+         'C:/siletz/scripts/R/reduce_qlc.R',
+         'C:/siletz/scripts/R/proc_routing.R'),
        source)
 
 qOut <- proc_flow_4_wq(wqFil)
-
-saveRDS(qOut, 'D:/siletz_tmp/scratch/wq/process_outflows.RData')
 
 # Reduce qOut to specified dates 
 # DELETE WHEN THIS IS ALL DONE!!!!##############################################
@@ -24,7 +24,7 @@ qRch <- reduce_qlc(strDte = '2004-10-01', endDte = '2004-12-01',
                    df2Red = qOut[['qRch']])
 # DELETE WHEN THIS IS ALL DONE!!!!##############################################
 
-emcdwcTmp <- read.csv('D:/siletz/emcdwc.csv') # Load EMC/DWC
+emcdwcTmp <- read.csv('C:/siletz/calib/wq/emcdwc.csv') # Load EMC/DWC
 
 # SET CONCENTRATIONS FOR HRUs/RUNOFF ----
 emcdwc <- data.frame(do.call(rbind, strsplit(names(qOut[['qLat']]), '_')))
@@ -79,20 +79,26 @@ latQ <- latQ[, c(1, nOrd$ord)]
 latL <- latL[, c(1, nOrd$ord)]
 latC <- latC[, c(1, nOrd$ord)]
 
-rchL <- rchC <- rchO <- rchV
+rchL <- rchC <- rchS <- rchE <- rchV
 
 # Initialize the df
 rchC[, 2 : length(rchC)] <- rchL[, 2 : length(rchL)] <- 0
 
+x1 <- x2 <- x3 <- x4 <- rchC
+
+JS <- 0.5
+
 # Convert reach flow rate (m3/s) to reach out volume (m3/dt)
-rchO[, 2 : length(rchO)] <- rchQ[, 2 : length(rchQ)] * 3600
+rchS[, 2 : length(rchS)] <- JS * rchQ[, 2 : length(rchQ)] * 3600
 
-names(rchQ) <- names(rchV) <- c('Date', paste0('Bas', 1 : 17))
+rchE[, 2 : length(rchE)] <- (1 - JS) * rchQ[, 2 : length(rchQ)] * 3600
 
-names(rchC) <- names(rchL) <- names(rchO) <- c('Date', paste0('Bas', 1 : 17))
+names(rchQ) <- names(rchV) <- names(rchC) <- c('Date', paste0('Bas', 1 : 17))
+
+names(rchE) <- names(rchL) <- names(rchS) <- c('Date', paste0('Bas', 1 : 17))
 
 # Import reach processing information
-shpFile <- 'D:/siletz/inputs/shp/basins.shp'
+shpFile <- 'C:/siletz/inputs/shp/basins.shp'
 
 lnks <- proc_routing(shpFile)
 
@@ -121,21 +127,25 @@ for (i in 2 : length(latQ)) {
 
     j0 = j1 - 1 # previous time step
 
-    # CONC = [IMAT + CONCS * (VOLS - SROVOL)] / (VOL - EROVOL)
+    # CONC = [IMAT + CONCS * (VOLS - SROVOL)] / (VOL + EROVOL)
     # Reach ouflow concentration
     # PAR    PAR           O CONV    Unt1    Unt2  
     # ----    ------------ - ----- ------    -----
-    IMAT   = IMAT[j1]              # kg   -> kg
-    CONCS  = rchC[j0, bcl] * 10^-3 # mg/L -> kg/m3
-    VOLS   = rchV[j0, bcl] * 10^6  # Mm3  -> m3
-    SROVOL = rchO[j0, bcl]         # m3   -> m3
-    VOL    = rchV[j1, bcl] * 10^6  # Mm3  -> m3
-    EROVOL = rchO[j1, bcl]         # m3   -> m3
+    xIMAT   = IMAT[j1]              # kg   -> kg
+    xCONCS  = rchC[j0, bcl] * 10^-3 # mg/L -> kg/m3
+    xVOLS   = rchV[j0, bcl] * 10^6  # Mm3  -> m3
+    xSROVOL = rchS[j1, bcl]         # m3   -> m3
+    xVOL    = rchV[j1, bcl] * 10^6  # Mm3  -> m3
+    xEROVOL = rchE[j1, bcl]         # m3   -> m3
     # --------------------------------------------
 
-    CONC = (IMAT + CONCS * (VOLS - SROVOL)) / (VOL - EROVOL)
+    # CONC = (xIMAT + xCONCS * (xVOLS - xSROVOL)) / (xVOL + xEROVOL)
+    x1[j1, bcl] <- (xVOLS - xSROVOL)
+    x2[j1, bcl] <- xCONCS * (xVOLS - xSROVOL)
+    x3[j1, bcl] <- txNmr <- xIMAT + xCONCS * (xVOLS - xSROVOL)
+    x4[j1, bcl] <- txDmr <- (xVOL + xEROVOL)
 
-    rchC[j1, bcl] <- CONC * 10^3 # Convert from kg/m3 back to mg/L
+    rchC[j1, bcl] <- txNmr / txDmr * 10^3 # Convert from kg/m3 back to mg/L
     
     # Reach ouflow mass
 
